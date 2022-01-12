977dbf1ece843bd726950be9f45be2c79eec5f3f
Mubashshir <ahmubashshir@gmail.com>
2022-01-12 23:44:19 +0600

    Update build steps
    Signed-off-by: Mubashshir <ahmubashshir@gmail.com>

--- helper/badger.js
+++ helper/badger.js
@@ -1,29 +1,33 @@
 import makeBadge from '../node_modules/badge-maker/lib/make-badge.js';
 import simpleIcons from 'simple-icons';
-import { optimize } from 'svgo';
+import {
+	optimize
+} from 'svgo';
 import badges from './badges.js';
 import fs from 'fs';
 import path from 'path';
 import docker from 'is-docker'
 const noop = () => {}
-const root = './../'
+const root = '.'
 const dir = path.join(
-				root,
-				docker()?'public':'static',
-				'/assets/badges'
-			);
+	root,
+	docker() ? 'public' : 'static',
+	'/assets/badges'
+);
 
 let badgeInfo = {};
-
-const size = (data) => {
-	return Math.round((data / 1024) * 1000) / 1000 + ' KiB';
+console.log( dir )
+const size = ( data ) => {
+	return Math.round( ( data / 1024 ) * 1000 ) / 1000 + ' KiB';
 }
 
-const profit = (size, optisize) => {
-	return Math.abs(Math.round((100 - (optisize * 100) / size) * 10) / 10);
+const profit = ( size, optisize ) => {
+	return Math.abs( Math.round( ( 100 - ( optisize * 100 ) / size ) * 10 ) / 10 );
 }
 
-fs.mkdirSync(dir ,{ recursive: true });
+fs.mkdirSync( dir, {
+	recursive: true
+} );
 fs.readdirSync( dir, ( err, files ) => {
 	if ( err ) throw err;
 	for ( const file of files ) {
@@ -37,7 +41,7 @@ badges.forEach(
 		let _start = Date.now();
 		let _path = path.join( dir, badge.name + '.svg' );
 		let logo = simpleIcons.get( badge.icon || badge.name );
-		console.log(`${badge.name}:`);
+		console.log( `${badge.name}:` );
 
 		let badgeData = {
 			format: 'svg',
@@ -52,23 +56,25 @@ badges.forEach(
 		}
 
 		let _badge = makeBadge( badgeData );
-		let _size = Buffer.byteLength(_badge, 'utf8');
-		_badge = optimize(_badge, {
+		let _size = Buffer.byteLength( _badge, 'utf8' );
+		_badge = optimize( _badge, {
 			path: _path,
 			multipass: true,
-		});
+		} );
 		let _end = Date.now();
-		let _optisize = Buffer.byteLength(_badge.data, 'utf8');
-		
-		fs.writeFile(_path, _badge.data, 'utf8', noop);
-		badgeInfo[badge.name] = _badge.info;
-		console.log(`  optimize: ${_end - _start}ms`);
-		console.log(`    before: ${size(_size)}`)
-		console.log(`     after: ${size(_optisize)}`);
-		console.log(`     ratio: ${profit(_size, _optisize)}%\n`)
+		let _optisize = Buffer.byteLength( _badge.data, 'utf8' );
+
+		fs.writeFile( _path, _badge.data, 'utf8', noop );
+		badgeInfo[ badge.name ] = _badge.info;
+		console.log( `  optimize: ${_end - _start}ms` );
+		console.log( `    before: ${size(_size)}` )
+		console.log( `     after: ${size(_optisize)}` );
+		console.log( `     ratio: ${profit(_size, _optisize)}%\n` )
 
 	}
 )
 
-fs.mkdirSync(path.join(root, 'data') ,{ recursive: true });
-fs.writeFileSync(path.join(root, 'data/badge.json'), JSON.stringify(badgeInfo), 'utf8');
+fs.mkdirSync( path.join( root, 'data' ), {
+	recursive: true
+} );
+fs.writeFileSync( path.join( root, 'data/badge.json' ), JSON.stringify( badgeInfo ), 'utf8' );
---
+++ helper/badges.js
@@ -0,0 +1,60 @@
+export default [
+	{
+		"name": "anilist",
+		"color": "02A9FF"
+  },
+	{
+		"name": "discord",
+		"color": "7289DA"
+  },
+	{
+		"name": "github",
+		"color": "181717"
+  },
+	{
+		"name": "pgp",
+		"icon": "gnuprivacyguard",
+		"color": "0093DD"
+  },
+	{
+		"name": "kitsu",
+		"color": "FD755C"
+  },
+	{
+		"name": "matrix",
+		"color": "000000"
+  },
+	{
+		"name": "myanimelist",
+		"color": "2E51A2"
+  },
+	{
+		"name": "reddit",
+		"color": "FF4500"
+  },
+	{
+		"name": "stackoverflow",
+		"color": "F58025"
+  },
+	{
+		"name": "telegram",
+		"color": "26A5E4"
+  },
+	{
+		"name": "trakt",
+		"color": "ED1C24"
+  },
+	{
+		"name": "twitter",
+		"color": "1DA1F2"
+  },
+	{
+		"name": "irc",
+		"color": "00AC47",
+		"icon": "livechat"
+  },
+	{
+		"name": "goodreads",
+		"color": "372213"
+  }
+]
