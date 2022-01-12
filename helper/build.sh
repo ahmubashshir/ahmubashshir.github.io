b0b7e7aca178009565a8e0a7cd84b70064667f84
Mubashshir <ahmubashshir@gmail.com>
2022-01-13 00:12:06 +0600

    Update serviceWork register
    Signed-off-by: Mubashshir <ahmubashshir@gmail.com>

--- helper/build.sh
+++ helper/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 npm install
 npm run badge
+git config core.quotePath false
 hugo --environment production --minify --log --verboseLog
+# tar --dereference --hard-dereference --directory public -cvf artifact.tar .
