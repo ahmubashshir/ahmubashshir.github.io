8536c134473938f4a900f1416f99b61d350879c0
Mubashshir <ahmubashshir@gmail.com>
2022-01-13 16:32:20 +0600

    Update service worker
    Signed-off-by: Mubashshir <ahmubashshir@gmail.com>

--- assets/sw.js
+++ assets/sw.js
@@ -4,14 +4,14 @@ which is in turn a modified version of Jeremy Keith's service worker (https://ad
 with a few additional edits borrowed from Filament Group's. (https://www.filamentgroup.com/sw.js)
 */
 
-(function() {
-  const version = '{{ .GitInfo.AbbreviatedHash }}';
-  const cacheName = version + '::blog:';
+( function () {
+	const version = '{{ slicestr ((.GitInfo.AuthorDate | time.AsTime).Unix | sha1) 0 7 }}';
+	const cacheName = version + '::blog:';
 
-  const staticCacheName = cacheName + 'static';
-  const pagesCacheName = cacheName + 'pages';
+	const staticCacheName = cacheName + 'static';
+	const pagesCacheName = cacheName + 'pages';
 
-  const staticAssets = [
+	const staticAssets = [
     '/',
     '/assets/css/stylesheet.min.css',
     '/assets/js/highlight.min.js',
@@ -28,115 +28,117 @@ with a few additional edits borrowed from Filament Group's. (https://www.filamen
     '/404.html'
   ];
 
-  function updateStaticCache() {
-    // These items must be cached for the Service Worker to complete installation
-    return caches.open(staticCacheName)
-    .then(cache => {
-      return cache.addAll(staticAssets.map(url => new Request(url, {credentials: 'include'})));
-    });
-  }
+	function updateStaticCache() {
+		// These items must be cached for the Service Worker to complete installation
+		return caches.open( staticCacheName )
+			.then( cache => {
+				return cache.addAll( staticAssets.map( url => new Request( url, {
+					credentials: 'include'
+				} ) ) );
+			} );
+	}
 
-  function getCacheForPath(path) {
-    if (staticAssets.includes(path) === true || path.indexOf('/assets/') === 0) {
-      return staticCacheName;
-    }
-    return pagesCacheName;
-  }
+	function getCacheForPath( path ) {
+		if ( staticAssets.includes( path ) === true || path.indexOf( '/assets/' ) === 0 ) {
+			return staticCacheName;
+		}
+		return pagesCacheName;
+	}
 
-  function stashInCache(cacheName, request, response) {
-    caches.open(cacheName)
-    .then(cache => cache.put(request, response));
-  }
+	function stashInCache( cacheName, request, response ) {
+		caches.open( cacheName )
+			.then( cache => cache.put( request, response ) );
+	}
 
-  // Limit the number of items in a specified cache.
-  function trimCache(cacheName, maxItems) {
-    caches.open(cacheName)
-    .then(cache => {
-      cache.keys()
-      .then(keys => {
-        if (keys.length > maxItems) {
-          cache.delete(keys[ 0 ])
-          .then(trimCache(cacheName, maxItems));
-        }
-      });
-    });
-  }
+	// Limit the number of items in a specified cache.
+	function trimCache( cacheName, maxItems ) {
+		caches.open( cacheName )
+			.then( cache => {
+				cache.keys()
+					.then( keys => {
+						if ( keys.length > maxItems ) {
+							cache.delete( keys[ 0 ] )
+								.then( trimCache( cacheName, maxItems ) );
+						}
+					} );
+			} );
+	}
 
-  // Remove caches whose name is no longer valid
-  function clearOldCaches() {
-    return caches.keys()
-    .then(keys => {
-      return Promise.all(keys
-        .filter(key => key.indexOf(version) !== 0)
-        .map(key => caches.delete(key))
-       );
-    });
-  }
+	// Remove caches whose name is no longer valid
+	function clearOldCaches() {
+		return caches.keys()
+			.then( keys => {
+				return Promise.all( keys
+					.filter( key => key.indexOf( version ) !== 0 )
+					.map( key => caches.delete( key ) )
+				);
+			} );
+	}
 
-  // Events!
-  self.addEventListener('message', event => {
-    if (event.data.command === 'trimCaches') {
-      trimCache(pagesCacheName, 35);
-    }
-  });
+	// Events!
+	self.addEventListener( 'message', event => {
+		if ( event.data.command === 'trimCaches' ) {
+			trimCache( pagesCacheName, 35 );
+		}
+	} );
 
-  self.addEventListener('install', event => {
-    event.waitUntil(updateStaticCache()
-      .then(() => self.skipWaiting())
-     );
-  });
+	self.addEventListener( 'install', event => {
+		event.waitUntil( updateStaticCache()
+			.then( () => self.skipWaiting() )
+		);
+	} );
 
-  self.addEventListener('activate', event => {
-    event.waitUntil(clearOldCaches()
-      .then(() => self.clients.claim())
-     );
-  });
+	self.addEventListener( 'activate', event => {
+		event.waitUntil( clearOldCaches()
+			.then( () => self.clients.claim() )
+		);
+	} );
 
-  self.addEventListener('message', event => {
-    if (event.data.command === 'trimCaches') {
-      trimCache(pagesCacheName, 35);
-    }
-  });
+	self.addEventListener( 'message', event => {
+		if ( event.data.command === 'trimCaches' ) {
+			trimCache( pagesCacheName, 35 );
+		}
+	} );
 
-  self.addEventListener('fetch', event => {
-    const request = event.request;
-    const url = new URL(request.url);
+	self.addEventListener( 'fetch', event => {
+		const request = event.request;
+		const url = new URL( request.url );
 
-    if (url.href.indexOf('{{ $.Site.BaseURL }}') !== 0) {
-      return;
-    }
+		if ( url.href.indexOf( '{{ $.Site.BaseURL }}' ) !== 0 ) {
+			return;
+		}
 
-    // Ignore non-GET requests
-    if (request.method !== 'GET') {
-      return;
-    }
+		// Ignore non-GET requests
+		if ( request.method !== 'GET' ) {
+			return;
+		}
 
-    // Ignore query-stringâ€™d requests
-    if (url.href.indexOf('?') !== -1) {
-      return;
-    }
+		// Ignore query-stringâ€™d requests
+		if ( url.href.indexOf( '?' ) !== -1 ) {
+			return;
+		}
 
-    // Try the network first, fall back to the cache, finally the offline page (for HTML requests)
-    event.respondWith(
-      fetch(request)
-      .then(response => {
-        // NETWORK
-        // Stash a copy of this page in the pages cache
-        const copy = response.clone();
-        stashInCache(getCacheForPath(url.pathname), request, copy);
-        return response;
-      })
-      .catch(() => {
-        // CACHE or FALLBACK
-        if (request.headers.get('Accept').indexOf('text/html') !== -1) {
-          return caches.match(request)
-            .then(response => response || caches.match('/offline/'));
-        } else {
-          return caches.match(request).then(response => response);
-        }
-      })
-    );
-    return;
+		// Try the network first, fall back to the cache, finally the offline page (for HTML requests)
+		event.respondWith(
+			fetch( request )
+			.then( response => {
+				// NETWORK
+				// Stash a copy of this page in the pages cache
+				const copy = response.clone();
+				stashInCache( getCacheForPath( url.pathname ), request, copy );
+				return response;
+			} )
+			.catch( () => {
+				// CACHE or FALLBACK
+				if ( request.headers.get( 'Accept' ).indexOf( 'text/html' ) !== -1 ) {
+					return caches.match( request )
+						.then( response => response || caches.match( '/offline/' ) );
+				} else {
+					return caches.match( request ).then( response => response );
+				}
+			} )
+		);
+		return;
 
-  });
-})();
+	} );
+} )();
